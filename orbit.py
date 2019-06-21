a9a2890e10283319190c0a314ee1c8382af09b4e
sourkey <34192401+sourkey@users.noreply.github.com>
2019-06-21 14:44:26 -0400

    Update orbit.py

--- orbit.py
+++ orbit.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+@author: sourkey
+
+"""
 
 import math
 import numpy as np
@@ -8,6 +13,7 @@ class Orbit:
     _inOrbit = []
     _inOrbitCount = 0
     
+    # object initialization
     def __init__(self, name=''):
         if name == '':
             Orbit._inOrbit.append(len(Orbit._inOrbit) + 1)
@@ -16,82 +22,51 @@ class Orbit:
 
         self.status = 0
         self.path = []
-        
+    
+    # summary of orbital objects    
     def showInOrbit(self):
         print('Items in orbit:',len(Orbit._inOrbit))
         print(*Orbit._inOrbit)
     
-        
+    # circular orbit calcs    
     def circularOrbit(self, smajor=4000, steps=50, table='hide', image='hide', summary='hide'):
         self.smajor = smajor
-        self.sminor = 0
         self.eccen = 0
         self.steps = steps
+        self.mass = 5.972*10**24
+        self.speed = 0
         
         # circular orbit - applies parametric equation for orbital motion
-        for a in range(0, self.steps):
+        self.speed = math.sqrt((6.674*10**-11) * self.mass / self.smajor / 1000)
+        print(self.speed)
+        for a in range(0, self.steps + 1):
             self.angle = a * 2 * math.pi / self.steps
-            self.path.append([self.angle, math.cos(self.angle) * smajor, math.sin(self.angle) * smajor])
-                  
+            self.path.append([self.angle,\
+                math.cos(self.angle) * smajor, math.sin(self.angle) * smajor, self.smajor, self.speed])
+        
+        # print table of positions at true anomoly          
         if table == 'show':
-            for pa, px, py in self.path:
-                print('{0:8.2f}\t{1:8.2f}\t{2:8.2f}'.format(pa, px, py))
+            print('Anomoly [rad]\tX [km]\t\tY [km]\t\tR [km]\t\tVelocity [m/s]')
+            print('-' * 80)
+            for pa, px, py, pr, pv in self.path:
+                print('{0:8.2f}\t{1:8.2f}\t{2:8.2f}\t{3:8.2f}\t{4:8.2f}'.format(pa, px, py, pr, pv))
 
+        # show image of orbit
         if image == 'show':
-            pass
+            fig = plt.figure()
+            fig.suptitle('Circular Orbit, R={0}'.format(self.smajor))
+            plt.plot(np.array(self.path)[:,1],np.array(self.path)[:,2], label='Path')
+            plt.plot(0,0, label='Center', marker='x')
+            plt.legend()
+            plt.show()
             
+            pass
+        
+        # print orbital paramenters
         if summary == 'show':           
             # print orbit details after calculation       
             print('------Orbital Parameters------')
             print('Major Axis:', self.smajor)
-            print('Minor Axis:', self.sminor)
             print('Eccentricity:', self.eccen)
             print('Steps:', self.steps)
-    
-            
-    def ellipticalOrbit(self, smajor=4000, sminor=0, e=0, steps=50, table='hide', image='hide', summary='hide'):
-        self.smajor = smajor
-        self.sminor = sminor
-        self.eccen = e
-        self.steps = steps
-            
-        # elliptical orbit
-        if self.sminor <= self.smajor and self.sminor > 0 and self.smajor > 0:
-            if self.eccen == 0:
-                self.eccen = math.sqrt(self.smajor**2 - self.sminor**2)/self.smajor
-            
-            for a in range(0, self.steps):
-                self.angle = a * 2 * math.pi / self.steps
-                self.path.append([self.angle, math.cos(self.angle) * smajor, math.sin(self.angle) * smajor])
-                
-        elif (self.sminor > 0 and self.eccen > 0 and self.eccen <= 1) or (self.smajor == 0 and self.eccen > 0 and self.eccen <= 1):
-            pass
-        else:
-            raise Exception('Check input arguments.')
-                           
-        if table == 'show':
-            for pa, px, py in self.path:
-                print('{0:8.2f}\t{1:8.2f}\t{2:8.2f}'.format(pa, px, py))
-            
-        if image == 'show':
-            pass
-            
-        if summary == 'show':
-            
-            # print orbit details after calculation       
-            print('------Orbital Parameters------')
-            print('Major Axis:', self.smajor)
-            print('Minor Axis:', self.sminor)
-            print('Eccentricity:', self.eccen)
-            print('Steps:', self.steps)
-    
-    def orbitParameters(self):
-        print('------Orbital Parameters------')
-        print('Major Axis:', self.major)
-        print('Minor Axis:', self.minor)
-        print('Eccentricity:', self.eccen)
-        print('Steps:', self.steps)
-        
-    def orbitingObjects(self):
-        print('Objects in orbit:')
-        print(*self.inOrbit)
+            print('Orbital Period:')
